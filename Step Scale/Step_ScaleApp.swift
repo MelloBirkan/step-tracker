6101dc02d32bc04fce3139dac72b5cd8dd63fc8a
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2024-04-27 09:24:57 -0300

    Created Xcode project

---
+++ Step Scale/Step_ScaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_ScaleApp.swift
+//  Step Scale
+//
+//  Created by Marcello Gonzatto Birkan on 27/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_ScaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
